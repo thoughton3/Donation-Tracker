129b5906aed2ba34cbdda5b133581b64d88b2bdb
katevance <kate.vance@gatech.edu>
2018-09-23 19:08:31 -0400

    updating

--- app/src/main/java/edu/gatech/cs2340/donationtracker/LoginActivity.java
+++ app/src/main/java/edu/gatech/cs2340/donationtracker/LoginActivity.java
@@ -2,13 +2,17 @@ package edu.gatech.cs2340.donationtracker;
 import java.util.HashMap;
 
 public class LoginActivity {
-    private HashMap<String, String> userData = new HashMap<>();
+    private static HashMap<String, String> userData = new HashMap<>();
 
     public void addUser(User user) {
         userData.put(user.getUsername(), user.getPassword());
     }
 
-    public boolean login(User user) {
-        return (userData.get(user.getUsername()).equals(user.getPassword()));
+    public static boolean login(User user) {
+        if (userData.get(user.getUsername()) != null) {
+            return (userData.get(user.getUsername()).equals(user.getPassword()));
+        } else {
+            return false;
+        }
     }
 }
--- app/src/main/java/edu/gatech/cs2340/donationtracker/MainActivity.java
+++ app/src/main/java/edu/gatech/cs2340/donationtracker/MainActivity.java
@@ -1,21 +1,45 @@
 package edu.gatech.cs2340.donationtracker;
 
+import android.support.design.widget.BaseTransientBottomBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.support.design.widget.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText username;
+    private EditText password;
 
     @Override
     //start up
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
+        username = (EditText) findViewById(R.id.editText4);
+        password = (EditText) findViewById(R.id.editText3);
+        LoginActivity users = new LoginActivity();
+        users.addUser(new User("Thomas", "H"));
+    }
+
+    public void onLogin(View view) {
+        Snackbar snackbar = Snackbar.make(findViewById(R.id.Context), "Incorrect username and password combination", Snackbar.LENGTH_SHORT);
+        if (LoginActivity.login(new User(username.getText().toString(), password.getText().toString()))) {
+            setContentView(R.layout.activity_main);
+        } else {
+            snackbar.show();
+        }
+    }
+
+    public void onCancel(View view) {
+        setContentView(R.layout.welcomescreen);
     }
 
     public void onLogOut(View view) {
