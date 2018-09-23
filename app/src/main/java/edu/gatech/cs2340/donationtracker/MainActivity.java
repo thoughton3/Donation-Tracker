b87c0b22b198496bdd8131e7fb8342485785b97c
jtaetle <jtaetle@gatech.edu>
2018-09-23 16:22:00 -0400

    Merge branch 'master' of https://github.com/thoughton3/Thomas-and-the-Goyls-Donation-Tracker
    # Conflicts:
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/edu/gatech/cs2340/donationtracker/MainActivity.java
+++ app/src/main/java/edu/gatech/cs2340/donationtracker/MainActivity.java
@@ -16,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    public void onLogOut(View view) {
+        setContentView(R.layout.welcomescreen);
     }
 }
