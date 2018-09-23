aa7b9fcfafb451d3178a417cc90a5c1f7bf3007c
thoughton3 <32284477+thoughton3@users.noreply.github.com>
2018-09-23 14:03:14 -0400

    Donation Tracker File New Activity Creation

---
+++ app/src/main/java/edu/gatech/cs2340/donationtracker/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.gatech.cs2340.donationtracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    //start up
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
