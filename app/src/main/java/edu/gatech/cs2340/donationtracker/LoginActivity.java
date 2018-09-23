f4e48d51ec6c0f471c3252590c4515b1db2e23f5
katevance <kate.vance@gatech.edu>
2018-09-23 14:43:18 -0400

    User and LoginActivity created

--- app/src/main/java/edu/gatech/cs2340/donationtracker/LoginActivity.java
+++ app/src/main/java/edu/gatech/cs2340/donationtracker/LoginActivity.java
@@ -1,4 +1,10 @@
 package edu.gatech.cs2340.donationtracker;
+import java.util.HashMap;
 
 public class LoginActivity {
+    private HashMap<String, String> userData = new HashMap<>();
+
+    public void addUser(User user) {
+        userData.put(user.getUsername(), user.getPassword());
+    }
 }
