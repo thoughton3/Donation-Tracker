fcf3cf84453479b787885d44d8c9e430f244bf71
thoughton3 <32284477+thoughton3@users.noreply.github.com>
2018-09-30 22:32:31 -0400

    Commiting Register feature

--- app/src/main/java/edu/gatech/cs2340/donationtracker/LoginActivity.java
+++ app/src/main/java/edu/gatech/cs2340/donationtracker/LoginActivity.java
@@ -1,11 +1,15 @@
 package edu.gatech.cs2340.donationtracker;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class LoginActivity {
     private static HashMap<String, String> userData = new HashMap<>();
+    private static ArrayList<User> userList = new ArrayList<>();
 
-    public void addUser(User user) {
-        userData.put(user.getUsername(), user.getPassword());
+    public static void addUser(User user) {
+        if(userData.put(user.getUsername(), user.getPassword()) == null) {
+            userList.add(user);
+        }
     }
 
     public static boolean login(User user) {
@@ -14,5 +18,13 @@ public class LoginActivity {
         } else {
             return false;
         }
+    }
+
+    public static String getName(String username) {
+        return userList.get(userList.indexOf(new User(username, ""))).getName();
+    }
+
+    public static AccountType getAccountType(String username) {
+        return userList.get(userList.indexOf(new User(username, ""))).getAccountType();
     }
 }
--- app/src/main/java/edu/gatech/cs2340/donationtracker/MainActivity.java
+++ app/src/main/java/edu/gatech/cs2340/donationtracker/MainActivity.java
@@ -9,29 +9,44 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.support.design.widget.Snackbar;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText username;
     private EditText password;
+    private EditText firstName;
+    private EditText lastName;
+    private Spinner accountTypeSpinner;
+    private User user;
 
     @Override
     //start up
     protected void onCreate(Bundle savedInstanceState) {
         LoginActivity users = new LoginActivity();
-        users.addUser(new User("Thomas", "H"));
+        users.addUser(new User("Thomas Houghton","Thomas", "H", AccountType.ADMIN));
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcomescreen);
-
     }
 
     public void onLogin(View view) {
+        username = (EditText) findViewById(R.id.editText4);
+        password = (EditText) findViewById(R.id.editText3);
         Snackbar snackbar = Snackbar.make(findViewById(R.id.Context), "Incorrect username and password combination", Snackbar.LENGTH_SHORT);
         if (username != null && password != null && LoginActivity.login(new User(username.getText().toString(), password.getText().toString()))) {
+            user = new User(username.getText().toString(), "");
+            String userName = LoginActivity.getName(user.getUsername());
+            AccountType userAccountType = LoginActivity.getAccountType(user.getUsername());
             setContentView(R.layout.activity_main);
+            TextView welcomeMessage = findViewById(R.id.textView);
+            welcomeMessage.setText("Welcome " + userAccountType.getValue() + ": " + userName + "!");
         } else {
             snackbar.show();
         }
@@ -47,7 +62,28 @@ public class MainActivity extends AppCompatActivity {
 
     public void onWelcomeScreen(View view) {
         setContentView(R.layout.login);
-        username = (EditText) findViewById(R.id.editText4);
-        password = (EditText) findViewById(R.id.editText3);
     }
+
+    //Register button on Home screen
+    public void onRegisterNow(View view) {
+        setContentView(R.layout.register);
+        accountTypeSpinner = findViewById(R.id.spinner1);
+        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, AccountType.values());
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        accountTypeSpinner.setAdapter(adapter);
+    }
+
+    //Registers account passed in
+    public void onRegisterAccount(View view) {
+        username = (EditText) findViewById(R.id.editText5);
+        password = (EditText) findViewById(R.id.editText6);
+        firstName = (EditText) findViewById(R.id.editText);
+        lastName = (EditText) findViewById(R.id.editText2);
+        String name = firstName.getText().toString() + " " + lastName.getText().toString();
+        LoginActivity.addUser(new User(name, username.getText().toString(), password.getText().toString(), (AccountType) accountTypeSpinner.getSelectedItem()));
+        setContentView(R.layout.login);
+
+    }
+
+
 }
